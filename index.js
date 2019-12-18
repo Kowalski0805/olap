593a1d35d42526d7670509144b8c6d2eaabce13f
kowalski0805 <illya.fedorovych@gmail.com>
2019-12-18 19:45:23 +0200

    Arrests

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const map = {
   ARREST_DATE: dates,
   OFNS_DESC: offences,
   ARREST_BORO: boros,
-  ARREST_PRECINCT: precincts,
+  // ARREST_PRECINCT: precincts,
   PERP_AGE_GROUP: perp_ages,
   PERP_SEX: persp_sexes,
   PERP_RACE: perp_races,
@@ -17,18 +17,29 @@ const map = {
 const check = (fact, obj) => Object.keys(map).every(field => fact[field] == obj[field]);
 
 const inflate = obj => Object.keys(map).reduce((acc, field) => {
-  acc[field] = obj.field;
+  acc[field] = obj[field];
   return acc;
 }, {count: 1});
 
-
+let curr_date;
 let count = 0;
 const create = obj => {
+  if (!curr_date) curr_date = obj.ARREST_DATE.split('/')[0];
+  if (curr_date !== obj.ARREST_DATE.split('/')[0]) {
+    fs.writeFileSync('arrest' + curr_date + '.json', JSON.stringify(facts, null, 4));
+    facts.length = 0;
+    curr_date = obj.ARREST_DATE.split('/')[0];
+  }
+
+  obj.ARREST_DATE = obj.ARREST_DATE.replace(/\/../, '');
+
   for (const field in map) {
     if (!map[field].includes(obj[field])) map[field].push(obj[field]);
   }
+  
   count++;
   console.log(count, ' / ', 167965);
+  
   const field = facts.find(f => check(f, obj));
   if (field) field.count = field.count + 1;
   else facts.push(inflate(obj));
@@ -38,7 +49,9 @@ fs.createReadStream('arrest.csv')
   .pipe(csv())
   .on('data', (data) => create(data))
   .on('end', () => {
-    fs.writeFileSync('arrest.json', JSON.stringify(facts, null, 4));
+    fs.writeFileSync('arrest' + curr_date + '.json', JSON.stringify(facts, null, 4));
+
+    console.log('Done!');
     // [
     //   { NAME: 'Daffy Duck', AGE: '24' },
     //   { NAME: 'Bugs Bunny', AGE: '22' }
